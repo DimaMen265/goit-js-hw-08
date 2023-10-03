b1b29488bee704c8fe0a89c1053b9ca09c1be5fd
DimaMen265 <menzak87@gmail.com>
2023-10-03 15:41:45 +0200

    The end task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,46 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(galleryItem => {
+    const item = document.createElement("li");
+    item.classList.add("gallery__item");
+    
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.setAttribute("href", galleryItem.original);
+
+    const photo = document.createElement("img");
+    photo.classList.add("gallery__image");
+    photo.setAttribute("src", galleryItem.preview);
+    photo.setAttribute("alt", galleryItem.description);
+
+    link.appendChild(photo);
+    item.appendChild(link);
+    gallery.appendChild(item);
+});
+
+const lightbox = new SimpleLightbox(".gallery__item a", {
+    sourceAttr: "href",
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    disableScroll: false,
+    scrollZoom: false,
+    doubleTapZoom: false,
+});
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains("gallery__image")) {
+        lightbox.open(event.target);
+    }
+});
