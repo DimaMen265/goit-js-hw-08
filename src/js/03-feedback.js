3b421ab0cf8e048d178f41f9cc84d3abd42d8dd3
DimaMen265 <menzak87@gmail.com>
2023-10-05 18:40:52 +0200

    The end task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from "lodash.throttle";
 
+const formFeedback = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const localeStorageKey = "feedback-form-state";
+
+formFeedback.addEventListener("input", throttle(() => {
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+
+    localStorage.setItem(localeStorageKey, JSON.stringify(formData));
+}, 500));
+
+formFeedback.addEventListener("submit", event => {
+    event.preventDefault();
+    formFeedback.reset();
+});
+
+window.addEventListener("load", () => {
+    const formData = localStorage.getItem(localeStorageKey);
+    if (formData) {
+        const { email, message } = JSON.parse(formData);
+        emailInput.value = email;
+        messageInput.value = message;
+    };
+});
