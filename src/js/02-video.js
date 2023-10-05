26410fc53ae0c9246a0cf86b43d8a5d1eafd26fd
DimaMen265 <menzak87@gmail.com>
2023-10-05 14:44:24 +0200

    The end task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoPlayer = document.querySelector("#vimeo-player");
+const player = new Player(videoPlayer);
+
+const saveTime = currentTime => {
+    localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+const restoreTime = () => {
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+    if (savedTime !== null) {
+        return parseFloat(savedTime);
+    };
+
+    return null;
+};
+
+player.on("timeupdate", throttle(event => {
+    const currentTime = event.seconds;
+    saveTime(currentTime);
+}, 1000));
+
+const initialTime = restoreTime();
+if (initialTime !== null) {
+    player.setCurrentTime(initialTime);
+};
